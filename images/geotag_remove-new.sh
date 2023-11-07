a4de499cab290b849bf1f122655d17175477c4ce
Brian Wilson <wilson@bubba.org>
2023-11-06 20:00:44 -0500

    images

--- images/geotag_remove-new.sh
+++
@@ -1,8 +0,0 @@
-for i in *.*[jJpP][pPnN][gG]*; do 
-	find $i -mtime -2 | grep [jJpP][pPnN][gG]*
-	if [ $? -eq 0 ]; then
-		echo "Processing $i"
-	    exiftool -geotag= "$i";
-		rm -f *_original
-	fi
-done
--- images/geotag_remove.sh
+++
@@ -1,2 +0,0 @@
-for i in *.*[jJpP][pPnN][gG]*; do echo "Processing $i"; exiftool -geotag= "$i"; done
-rm -f *_original
